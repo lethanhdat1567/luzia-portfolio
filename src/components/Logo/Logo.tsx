f3ec4afb01263e0b1bfa0de895267a58b967d298
Le Thanh Dat <lethanhdat1567@gmail.com>
2025-12-23 21:18:43 +0700

    fix logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,7 +5,7 @@ function Logo() {
         <Link href={"/"}>
             <div className="">
                 <p className="cursor-pointer text-xl font-medium tracking-tight text-gray-900 transition duration-500 hover:opacity-40">
-                    TuanPhatSKL
+                    Tuấn Phát
                 </p>
             </div>
         </Link>
